9fdf82d46f6f4198405d490a2a0df75730f6672d
melzak252 <melzacki.jakub@gmail.com>
2024-08-23 02:15:14 +0200

    Fix fetch data api call

--- client/src/consumable/useAuth.ts
+++ client/src/consumable/useAuth.ts
@@ -7,12 +7,11 @@ const user = ref<{ username: string; email: string } | null>(null);
 export function useAuth() {
   const fetchUserData = async (): Promise<boolean> => {
     try {
-      const response = await axios.get('/users/me', {
+      const response = await axios.get('https://jmelzacki.com/api/users/me', {
         withCredentials: true
       });
       let apiUser = response.data;
       user.value = apiUser; // Set the username from the response'
-      console.log(apiUser)
       return !!apiUser;
     } catch (error) {
       console.error('Failed to fetch user data:', error);
