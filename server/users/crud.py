a94d6243f95c39c159970bd7f8fd12887be44477
melzak252 <melzacki.jakub@gmail.com>
2024-09-05 03:35:31 +0200

    Add alembic

--- server/users/crud.py
+++ server/users/crud.py
@@ -3,8 +3,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 
 async def add_base_permissions(session: AsyncSession):
-    admin = PermissionRepository(session).get_by_name("admin")
+    admin = await PermissionRepository(session).get_by_name("admin")
     if admin:
         return
 
-    PermissionRepository(session).create_permission("admin")
+    await PermissionRepository(session).create_permission("admin")
